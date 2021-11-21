257dd08128b0e29971adea8fa5d0394f36086af0
JimShaunUK <64266900+JimShaunUK@users.noreply.github.com>
2021-11-21 17:53:54 +0000

    Added Auth, registration and Products

---
+++ CoffeeAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CoffeeAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Products> Products { get; set; }
+    }
+}
---
+++ CoffeeAPI/Models/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeAPI.Models
+{
+    public class Products
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+        public string Description { get; set; }
+
+
+
+    }
+}
